76005d2d145f668e6b19e05a0d4907af35c8c23a
dmitry_khudzhanov <dima_khudzhanov@mail.ru>
2024-10-09 20:06:18 +0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Дима Худжанов on 09.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
